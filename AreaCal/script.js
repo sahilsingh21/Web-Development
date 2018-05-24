526d6489bd8f3bcb79e0a63e31c595659df20029
sahilsingh21 <ss3842486@gmail.com>
2018-05-25 02:05:09 +0530

    Problem solve src

--- AreaCal/script.js
+++ AreaCal/script.js
@@ -2,6 +2,5 @@ function calc(){
     var w = document.getElementById("width").value;
     var h = document.getElementById("height").value;
     var area = w * h;
-    alert("the area is: ", area);
     document.getElementById("answer").value = area;
 }
