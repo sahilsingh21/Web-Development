4ed91be0aa276ddd7bb68d720352a991c2f36f46
sahilsingh21 <ss3842486@gmail.com>
2021-05-29 09:13:26 +0530

    adding the middleware function like app.use and auth

--- BuildRestFullAPI/app.js
+++ BuildRestFullAPI/app.js
@@ -2,10 +2,24 @@ const express = require('express');
 
 const app = express();
 
+//Middlewares  (hitting the routes)
+//app.use(auth)  //this func. is the user is authentification or not
+app.use('/posts', () => {
+    console.log('This is a middleware running');
+});
 
-//Routes
+
+
+//Routes (if we send the messege then we use app.post('/', (req, res)....))
+//app.delete() and in that we write logic what post we can delete
+//app.patch()  it is updating the post
+
 app.get('/', (req, res) => {
     res.send('We are on home');
+});
+
+app.get('/posts', (req, res) => {
+    res.send('We are on posts');
 });
 
 //How to we start listing to server
